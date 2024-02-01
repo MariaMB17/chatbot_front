4d7ee27db9d3612ddf23a9df7414e6051cf8583e
Ciberyely <ciberyely@hotmail.com>
2024-02-01 13:45:36 -0400

    Depurado codigo

--- app/dashboard/chats/page.tsx
+++ app/dashboard/chats/page.tsx
@@ -2,7 +2,7 @@ import { getDataTextContex } from "@/app/lib/data-knowledge";
 import ChatNew from "@/app/ui/chats/chat-new";
 
 const Chat = async () => {
-    const textContext = await getDataTextContex(22);
+    const textContext = await getDataTextContex(1);
     return (
         <main>
             <ChatNew textContext={textContext} />
